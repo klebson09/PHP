77686dd1d516e4aa2556fe091e67499c988cb8ff
klebson09 <silvaklebson_@hotmail.com>
2014-06-16 17:47:51 -0300

    Revert "Arquivos básicos da criação do sistema"
    This reverts commit 24c9b43b845b4b98cfaa607b88121faeb4f75f35.

--- Controle_de_Tonner_IEL/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$host = "localhost";
-$banco = "controle_impressora";
-$usuario = "root";
-$senha = "";
-$conexao_bd = mysql_pconnect($host, $usuario, $senha) or trigger_error(mysql_error(),E_USER_ERROR); 
-?>
